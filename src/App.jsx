5246fb91f0450c0b76adbe5e0163436c19e95e30
AARAV PANDEY <142007358+AARAV948@users.noreply.github.com>
2025-05-22 15:55:12 +0530

    reset button added

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,18 @@ function App(){
         <h1>Count {value} </h1>   
         <button onClick={ 
           () => {
-            setValue(value+100);
+            setValue(value+1);
           }
         }>Add</button>
+
+        <br></br>
+        <br></br>
+
+         <button onClick={ 
+          () => {
+            setValue(0);
+          }
+        }>Reset</button>
 
 
       </> 
